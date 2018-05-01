f3c4a6a48ffdfbe54c01f3ad2d80b3973230f27b
Laurence MacGuire <leprechaun@gmail.com>
2018-05-01 22:17:20 +0700

    Stick to jobs by state, for now

--- gocd_prometheus.py
+++ gocd_prometheus.py
@@ -1,6 +1,7 @@
 import urllib3
 urllib3.disable_warnings()
 
+import yaml
 import traceback
 import requests
 import os
@@ -14,6 +15,9 @@ from prometheus_client import start_http_server
 from prometheus_client import Histogram
 from prometheus_client import Counter
 from prometheus_client import Summary
+from prometheus_client import Gauge
+
+import xml.etree.ElementTree as ET
 
 go = Yagocd(
 	server=os.getenv("GOCD_URL"),
@@ -23,118 +27,83 @@ go = Yagocd(
 	}
 )
 
+start_http_server(8000)
 
-group_count = Counter('gocd_group_count', 'gocd group count', ["pipeline_group"])
-pipeline_count = Counter('gocd_pipeline_count', 'gocd pipeline count', ["pipeline_group", "pipeline", "pipeline_key"])
-stage_count = Counter('gocd_pipeline_stage_count', 'gocd stage count', ["pipeline_group", "pipeline", "stage", "result", "pipeline_key", "stage_key"])
-job_count = Counter('gocd_pipeline_stage_job_counter', 'gocd job count', ["pipeline_group", "pipeline", "stage", "job", "pipeline_key", "stage_key", "job_key", "state"])
-job_duration = Summary('gocd_pipeline_stage_job_duration', 'length of jobs running on gocd', ["pipeline_group", "pipeline", "stage", "job", "pipeline_key", "stage_key", "job_key"])
-start_http_server( int(os.getenv("PROMETHEUS_PORT") or 8000) )
+# go.stages.get(pipeline_name="nginx-static-app-build", pipeline_counter="145", stage_name="build-image", stage_counter="3").jobs()
 
-largest_id_by_job = {}
-is_first_run = True
+watched = set([])
+
+watched_jobs = set([])
+
+job_count_by_state = Gauge('gocd_job_count_by_state', 'Number of jobs with status', ["state"])
 
 while True:
 	try:
-		for pipeline in go.pipelines:   # Iterate over all pipelines
-			try:
-				instance = pipeline.last()
-				if instance is None:
-					continue
+		tree = ET.fromstring(requests.get('https://gocd.k8s.fscker.org/go/cctray.xml').text)
+		#tree = ET.fromstring(cctray.text)
 
-				for stage in instance:  # Iterate over each stages of some pipeline instance
-					for job in stage:   # Iterate over each job of some pipeline
-					# print([ pipeline.data.id ], [job.data.id, job.data.name, job.data.result]])
-						obj = {
-							"pipeline": {
-								"key": pipeline.group + "/" + pipeline.data.name,
-								"group": pipeline.group,
-								"name": pipeline.data.name,
-								"counter": instance.data.counter
-							},
-							"stage": {
-								"key": pipeline.group + "/" + pipeline.data.name + "/" + stage.data.name,
-								"name": stage.data.name,
-								"counter": stage.data.counter,
-								"result": stage.data.result,
-							},
-							"job": {
-								"key": pipeline.group + "/" + pipeline.data.name + "/" + stage.data.name + "/" + job.data.name,
-								"name": job.data.name,
-								"id": job.data.id,
-								"state": job.data.state
-							}
-						}
+		for project in tree.findall("Project"):
+			#print(project.attrib)
+			# {'lastBuildLabel': '0.1-21-97dfc48d', 'name': 'gocd-exporter-build :: build-image', 'activity': 'Sleeping', 'lastBuildStatus': 'Success', 'webUrl': 'https://gocd.k8s.fscker.org/go/pipelines/gocd-exporter-build/21/build-image/1', 'lastBuildTime': '2018-04-28T10:42:45'}
+			if project.attrib["activity"] != "Sleeping":
+				pipeline = project.attrib["name"].split(" :: ")
+				if len(pipeline) == 3:
+					parsedLink = project.attrib["webUrl"].split("/")
+					# print project.attrib["name"] + " " + project.attrib["activity"] + " " + parsedLink[8] + " " + parsedLink[10]
+					to_add = (
+						pipeline[0],
+						pipeline[1],
+						pipeline[2],
+						parsedLink[8],
+						parsedLink[10]
+					)
+					if to_add not in watched:
+						#print("add", to_add)
+						watched.add(to_add)
+
+		#go.pipelines["nginx-static-app-deploy-prod"][67]["dummer-stage"]
+		to_remove = set([])
 
 
-						if job.data.state == "Completed":
-							duration = job.properties["cruise_job_duration"]
-						else:
-							duration = 0
+		job_counts_by_state = {
+			"Scheduled": 0,
+			"Assigned": 0,
+			"Preparing": 0,
+			"Building": 0,
+			"Completed": 0
+		}
 
-						obj["job"]["duration"] = int(duration)
-						print(obj["job"]["key"] + " - " + str(job.data.state))
+		for i in watched:
+			#print(stage.data.result, stage.data)
+			pipeline = go.pipelines[ i[0] ]
+			instance = pipeline[ i[3] ]
+			stage = instance[ i[1] ]
+			jobs = go.stages.get(
+				pipeline_name = pipeline.data.name,
+				pipeline_counter = instance.data.counter,
+				stage_name = stage.data.name,
+				stage_counter = stage.data.counter
+			).jobs()
 
-						if not is_first_run:
-							if obj["job"]["id"] > largest_id_by_job.get( obj["job"]["key"], 0 ):
-								print(" - ".join( [obj["job"]["key"], str(obj["job"]["duration"])]))
-								largest_id_by_job[ obj["job"]["key"] ] = obj["job"]["id"]
-								group_count.labels(
-										pipeline_group = obj["pipeline"]["group"]
-								).inc(1)
+			for job in jobs:
+				#print(pipeline.data.name, stage.data.name, job.data.name, job.data.state, job.data.result)
+				job_counts_by_state[job.data.state] += 1
 
-								pipeline_count.labels(
-										pipeline_group = obj["pipeline"]["group"],
-										pipeline = obj["pipeline"]["name"],
-										pipeline_key = obj["pipeline"]["key"]
-								).inc(1)
+			if stage.data.result in ["Passed", "Cancelled", "Failed"]:
+				to_remove.add(i)
+		
+		for i in to_remove:
+			s = go.pipelines[ i[0] ][ i[3] ][ i[1] ]
+			#print("remove", i, s.data.result )
+			#print(s.data)
+			#print('remove', i)
+			watched.remove(i)
 
-								stage_count.labels(
-									pipeline_group = obj["pipeline"]["group"],
-									pipeline = obj["pipeline"]["name"],
-									stage = obj["stage"]["name"],
-									result = obj["stage"]["result"],
-									pipeline_key = obj["pipeline"]["key"],
-									stage_key = obj["stage"]["key"]
-								).inc(1)
+		for state in job_counts_by_state:
+			job_count_by_state.labels(
+				state=state
+			).set(job_counts_by_state[state])
 
-								job_count.labels(
-									pipeline_group = obj["pipeline"]["group"],
-									pipeline = obj["pipeline"]["name"],
-									stage = obj["stage"]["name"],
-									job = obj["job"]["name"],
-									pipeline_key = obj["pipeline"]["key"],
-									stage_key = obj["stage"]["key"],
-									job_key = obj["job"]["key"],
-									state =  obj["job"]["state"]
-								).inc(1)
-
-
-								job_duration.labels(
-									pipeline_group = obj["pipeline"]["group"],
-									pipeline = obj["pipeline"]["name"],
-									stage = obj["stage"]["name"],
-									job = obj["job"]["name"],
-									pipeline_key = obj["pipeline"]["key"],
-									stage_key = obj["stage"]["key"],
-									job_key = obj["job"]["key"]
-								).observe(
-									obj["job"]["duration"]
-								)
-							else:
-								pass
-			except TypeError:
-				raise
-				#traceback.print_tb(sys.exc_info()[2])
-				#pass
-			except:
-				print "Unexpected error:", sys.exc_info()[0]
-				raise
-	except requests.exceptions.ConnectionError:
-		print "Unexpected error:", sys.exc_info()[0]
-		#raise
+		time.sleep(1)
+	except(requests.exceptions.ConnectionError):
 		pass
-
-	print("looped")
-	is_first_run = False
-	time.sleep( int(os.getenv("SAMPLE_FREQUENCY") or 10) )
